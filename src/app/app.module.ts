684c6c9dc7b9758891396a1947d011d1db96a4ed
Tom Chege <emdeechege@gmail.com>
2018-08-16 16:42:42 +0300

    Initial files created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MasterComponent } from './master/master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterComponent
   ],
   imports: [
     BrowserModule
