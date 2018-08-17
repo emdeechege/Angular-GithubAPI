b50e0fa3a06984fe56cde956b1ec0c535f9e8677
Tom Chege <emdeechege@gmail.com>
2018-08-17 13:24:00 +0300

    defining search parameters

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -9,20 +9,37 @@ import { ApiService } from '../api.service';
 })
 export class MasterComponent implements OnInit {
 
-  user1: any =[];
+  user1: any = [];
+  repos: any = [];
+  userName: string;
 
 
-  constructor(public apiservice: ApiService){
-//obj data will return data from url called on by the fn defined in service
-      this.apiservice.getUserDetails().subscribe(users => {
-            this.user1 = users;
+  constructor(public apiservice: ApiService) {
+    //obj data will return data from url called on by the fn defined in service
+    this.apiservice.getUserDetails().subscribe(users => {
+      this.user1 = users;
       console.log(this.user1);
     });
     this.apiservice.getRepos().subscribe(result => {
-          this.user1 = result;
-    console.log(this.user1);
+      this.user1 = result;
+      console.log(this.user1);
     });
 
+    searchUser() {
+      this.apiService.updateUser(this.userName);
+
+      this.apiService.apiProfile()
+        .subscribe(users => {
+          //console.log(res)
+          this.user1 = users;
+        })
+
+      this.apiService.getRepos()
+        .subscribe(result => {
+          //console.log(data)
+          this.repos = result;
+        })
+    }
   }
   ngOnInit() {
 
