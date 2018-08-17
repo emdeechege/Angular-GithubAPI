5ab393844e5b82e9f8be2639d15181df75ff8de3
Tom Chege <emdeechege@gmail.com>
2018-08-17 12:19:53 +0300

    card in place, data displaying

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-import { HttpClient } from '@angular/common/http'
+
 
 @Component({
   selector: 'app-master',
@@ -9,18 +9,15 @@ import { HttpClient } from '@angular/common/http'
 })
 export class MasterComponent implements OnInit {
 
-  users = [];
+  user1: any =[];
 
 
-  constructor(public apiservice: ApiService, public http: HttpClient) {
-    // this.getUser();
+  constructor(public apiservice: ApiService){
 
-
-  // getUser() {
-    this.apiservice.getUserDetails().subscribe(retObject => {
+      this.apiservice.getUserDetails().subscribe(users => {
       //obj data will return data from url called on by the fn defined in service
-      // this.users = userObject;
-      console.log(retObject);
+      this.user1 = users;
+      console.log(this.user1.login);
     });
   }
   ngOnInit() {
