743ca768141c2366c5225ba648fed6766aa306ed
Tom Chege <emdeechege@gmail.com>
2018-08-17 18:21:20 +0300

    landing page available

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -13,12 +13,17 @@ export class MasterComponent implements OnInit {
   user1: any = [];
   repos: any = [];
   userName: string;
-  // apiUrl: string = "https://api.github.com/users";
-  // apiKey: string = "1abd407493bf913907152b8557d281e9a561807a"
 
   constructor(public apiservice: ApiService, public http: HttpClient) {
     //obj data will return data from url called on by the fn defined in service
-
+    this.apiservice.getUserDetails().subscribe(users => {
+      this.user1 = users;
+      console.log(this.user1);
+    });
+    this.apiservice.getRepos().subscribe(result => {
+      this.repos = result;
+      console.log(this.repos);
+    });
   }
 
   findUser() {
