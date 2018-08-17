fb4fb77074903836f43d1ac8d42f848d6614206f
Tom Chege <emdeechege@gmail.com>
2018-08-17 17:37:34 +0300

    showing values

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-userName: string;
-apiUrl: string="https://api.github.com/users/";
-apiKey: string="1abd407493bf913907152b8557d281e9a561807a"
+  userName: string;
+  apiUrl: string = "https://api.github.com/users/";
+  apiKey: string = "1abd407493bf913907152b8557d281e9a561807a"
 
-  constructor(public http: HttpClient)
-   { this.userName= "emdeechege"}
+  constructor(public http: HttpClient) {
+  this.userName = 'emdeechege';
+  }
 
-  getUserDetails(){
-    return    this.http.get(this.apiUrl + this.userName + "?access_token=" + this.apiKey)
+  getUserDetails() {
+    return this.http.get(this.apiUrl + this.userName + "?access_token=" + this.apiKey)
   }
-  getRepos(){
-    return this.http.get(this.apiUrl + this.userName + "/repos" + "?access_token=" + this.apiKey)
+  getRepos() {
+    return this.http.get(this.apiUrl + this.userName + "/repos?access_token=" + this.apiKey)
   }
-  // searchUser(userName: string){
-  //   this.userName = userName;
+  updateUserName(userName: string) {
+    this.userName = userName;
 
   }
+}
--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -13,29 +13,33 @@ export class MasterComponent implements OnInit {
   user1: any = [];
   repos: any = [];
   userName: string;
-  apiUrl: string="https://api.github.com/u";
+  apiUrl: string="https://api.github.com/users";
   apiKey: string="1abd407493bf913907152b8557d281e9a561807a"
 
   constructor(public apiservice: ApiService, public http: HttpClient) {
     //obj data will return data from url called on by the fn defined in service
-    this.apiservice.getUserDetails().subscribe(users => {
-      this.user1 = users;
-      console.log(this.user1);
-    });
-    this.apiservice.getRepos().subscribe(result => {
-      this.repos = result;
-      console.log(this.repos);
-    });
+
   }
 
-  performSearch(userName: HTMLInputElement): void {//collects user data and adds the input the the api link and returns a new variable with the results. The results overide the trending gifs automatically.
-      var apiLink = (this.apiUrl + "userName" + "?access_token=" + this.apiKey);
-
-      this.http.get(apiLink).subscribe(response =>{
-          this.user1 = response;
-          console.log(this.user1);
-        });
-    }
+findUser(){
+  this.apiservice.updateUserName(this.userName);
+  this.apiservice.getUserDetails().subscribe(users => {
+    this.user1 = users;
+    console.log(this.user1);
+  });
+  this.apiservice.getRepos().subscribe(result => {
+    this.repos = result;
+    console.log(this.repos);
+  });
+}
+  // performSearch(userName: HTMLInputElement): void {
+  //     var apiLink = (this.apiUrl + "userName" + "?access_token=" + this.apiKey);
+  //
+  //     this.http.get(apiLink).subscribe(response =>{
+  //         this.user1 = response;
+  //         console.log(this.user1);
+  //       });
+  //   }
 
   ngOnInit() {
 
